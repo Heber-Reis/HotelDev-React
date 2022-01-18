55d05c0c6e170972d7f65418c8ade58218b7145f
Heber Magno <heber2014gbi@gmail.com>
2022-01-18 15:11:22 -0300

    criado estrutura de pastas

---
+++ pages/_app.jsx
@@ -0,0 +1,11 @@
+import "../src/style.css";
+
+function App ({ Component, pageProps }){
+  return(
+    <>
+      <Component {...pageProps}/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import styles from "./Header.module.css";
+
+function Header (){
+  return(
+    <div className="main">
+      <h1>Hotel Dev</h1>
+      <p>O seu paraiso na terra</p>
+      <button>Reservar agora!</button>
+    </div>
+  )
+}
+
+export default Header;
