c2a6f6bb3db36acad31dfafee7ba8412f613f61c
Heber Magno <heber2014gbi@gmail.com>
2022-01-19 00:18:12 -0300

    criado cabeçalho da página

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import "../src/style.css";
 
 function App ({ Component, pageProps }){
   return(
     <>
+      <Head>
+        <title>Hotel Dev</title>
+      </Head>
       <Component {...pageProps}/>
     </>
   )
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,10 +2,10 @@ import styles from "./Header.module.css";
 
 function Header (){
   return(
-    <div className="main">
+    <div className={styles.Header}>
       <h1>Hotel Dev</h1>
       <p>O seu paraiso na terra</p>
-      <button>Reservar agora!</button>
+      <button className={styles.button}>Reservar agora!</button>
     </div>
   )
 }
